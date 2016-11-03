43cf5be2dc8d41d8f0ae90e3b05f8cd187a136c6
markymark5127 <markmayne@ymail.com>
2016-11-03 15:08:49 -0400

    start methods wrote

--- AATree.cpp
+++ AATree.cpp
@@ -8,113 +8,188 @@
 /* **************************************************************** */
 
 #if CONSTRUCTOR || ALL
-//template <class T>
-//AATree<T>::AATree()
-//{
+template <class T>
+AATree<T>::AATree()
+{
     // TODO: Create sentinel node (i.e. bottomNode)
     // assign the sentinel node to root, lastNode, and deletedNode
-//}
+}
 #endif
 
 /* **************************************************************** */
 
 #if DESTRUCTOR || ALL
-//template <class T>
-//AATree<T>::~AATree()
-//{
+template <class T>
+AATree<T>::~AATree()
+{
     // TODO: Remove any nodes
     // don't forget to deallocate bottomNode
-//}
+}
 #endif
 
 /* **************************************************************** */
 
 #if ISEMPTY || ALL
 // TODO: isEmpty() method
+template <class T>
+bool AATree<T>::isEmpty()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FIND || ALL
 // TODO: find() method
+template <class T>
+bool AATree<T>::find(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FINDNODE || ALL
 // TODO: findNode() private method - iterative
+template <class T>
+Node<T> *AATree<T>::findNode(Node<T> *node, const T &x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FINDMIN || ALL
 // TODO: findMin() method
+template <class T>
+const T& AATree<T>::findMin()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FINDMINNODE || ALL
 // TODO: findMinNode() private method
+template <class T>
+Node<T> *AATree<T>::findMinNode(Node<T> *node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FINDMAX || ALL
 // TODO: findMax() method
+template <class T>
+const T& AATree<T>::findMax()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if FINDMAXNODE || ALL
 // TODO: findMaxNode() private method
+template <class T>
+Node<T> *AATree<T>::findMaxNode(Node<T> *node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if INSERT || ALL
 // TODO: insert() method
+template <class T>
+void AATree<T>::insert(const T &x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if INSERTNODE || ALL
 // TODO: insertNode() private method
+template <class T>
+bool AATree<T>::find(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if REMOVE || ALL
 // TODO: remove() method
+template <class T>
+void AATree<T>::insertNode(Node<T> *&node, const T &x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if REMOVENODE || ALL
 // TODO: removeNode() private method
+template <class T>
+void AATree<T>::removeNode(Node<T> *&node, const T &x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if SKEW || ALL
 // TODO: skew() private method
+template <class T>
+void AATree<T>::skew(Node<T> *&node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if SPLIT || ALL
 // TODO: split() private method
+template <class T>
+void AATree<T>::split(Node<T> *&node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if MAKEEMPTY || ALL
 // TODO: makeEmpty() method
+template <class T>
+void AATree<T>::makeEmpty()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if REMOVEALLNODES || ALL
 // TODO: removeAllNodes() private method
+template <class T>
+void AATree<T>::removeAllNodes(Node<T> *node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
